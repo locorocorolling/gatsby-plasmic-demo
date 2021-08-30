1535b6adb86c86bfac067e09cfef93b599007519
Kevin Loh <khaikhoon.loh@hotmail.com>
2021-08-31 00:07:21 +0800

    feat: automatically includes pages from Plasmic

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby Plasmic Demo",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "@plasmicapp/loader-gatsby",
+      options: {
+        projects: [
+          {
+            id: "5ztJncxm4GNHHZLjFtMN3i",  // ID of a project you are using
+            token: "ZvFrX7LXggA1dnkyrxbECzXX228PhpTvlIorAccxZjjbDEwjPYKA0trZq9xSPF5B5hdFBNOOx5mVVuaasWsg"  // API token for that project
+          },
+        ],
+        defaultPlasmicPage: require.resolve('./src/templates/default.tsx'),
+        // Fetches the latest revisions, whether or not they were unpublished!
+        // Disable for production to ensure you render only published changes.
+        preview: true
+      },
+    },
+  ],
 };
